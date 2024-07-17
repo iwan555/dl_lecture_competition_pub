fcb854084ef32f08ca1e6fa817cd22d555d0ee74
iwan555 <iwaiwa103eng@gmail.com>
2024-07-18 05:52:33 +0900

    Update eval.py

--- eval.py
+++ eval.py
@@ -10,7 +10,7 @@ from termcolor import cprint
 from tqdm import tqdm
 
 from src.datasets import ThingsMEGDataset
-from src.models import BasicConvClassifier
+from src.models import BasicConvClassifier_256_L4
 from src.utils import set_seed
 
 
@@ -31,7 +31,7 @@ def run(args: DictConfig):
     # ------------------
     #       Model
     # ------------------
-    model = BasicConvClassifier(
+    model = BasicConvClassifier_256_L4(
         test_set.num_classes, test_set.seq_len, test_set.num_channels
     ).to(args.device)
     model.load_state_dict(torch.load(args.model_path, map_location=args.device))
